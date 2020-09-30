a57aeda311280b2904dedcb8fab390ef2072a1ef
Jotham-Ntekim <jothamweb@gmail.com>
2020-09-30 06:12:18 -0700

    tried installing some packages to fix error

--- server/app.js
+++ server/app.js
@@ -44,5 +44,5 @@ app.use((err, req, res, next) => {
     }
 });
 
-
-app.listen(process.env.PORT, '0.0.0.0', () => console.log(`Server started at port : ${process.env.PORT}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started at port : ${process.env.PORT}`));
